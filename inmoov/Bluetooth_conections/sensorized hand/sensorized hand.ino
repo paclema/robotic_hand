6999facd26b6ce0c89985953b4d74268778bf57a
paclema <paclema@gmail.com>
2015-06-12 20:18:38 +0200

    Added Sensorized_hand sketch

---
+++ inmoov/Bluetooth_conections/sensorized hand/sensorized hand.ino
@@ -0,0 +1,21 @@
+const int delay_between_measaures = 250;
+void setup(){
+  Serial.begin(19200); //this baud is compatible with BT
+}
+void loop()
+{ 
+  Serial.print("A "); Serial.print(analogRead(A0));
+  Serial.print(" B "); Serial.print(analogRead(A1));
+  Serial.print(" C "); Serial.print(analogRead(A2));
+  Serial.print(" D "); Serial.print(analogRead(A3));
+  Serial.print(" E "); Serial.print(analogRead(A4));
+  Serial.println(); //empty line to see measures differences
+  delay(delay_between_measaures);
+  
+  /*
+  values seems to be like that:
+  450-500 - straight
+  600 - a little bit
+  800 - full
+  */
+}
