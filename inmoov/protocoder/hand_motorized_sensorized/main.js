296d894082d4da401bcba65468caa7dde6d59ef4
paclema <paclema@gmail.com>
2015-06-16 11:26:42 +0200

    Updated protocoder app

--- inmoov/protocoder/hand_motorized_sensorized/main.js
+++ inmoov/protocoder/hand_motorized_sensorized/main.js
@@ -1,18 +1,19 @@
 /* hand_controller
 
-Read from arduino by bluetooth diferent sensors and plot it on dashboard
+Read from arduino by bluetooth diferent sensors and plot it on phone
 
     
 */
 
 ui.backgroundColor(100, 100, 170);
-var txt = ui.addText(10, 1450, ui.screenWidth, 200);
-
+var txt = ui.addText(10, 1400, ui.screenWidth, 50);
+var txt2 = ui.addText(10, 1450, ui.screenWidth, 50);
 
 //var servo_values;
 var a,b,c,d,e;
-
-ui.addButton("Scan bluetooth", 10, 0).onClick(function() {
+var a_draw,b_draw,c_draw,d_draw,e_draw;
+// *******************************************************************    Bluetooth conections:
+ui.addButton("Scan bluetooth", 680, 150).onClick(function() {
     network.bluetooth.scanNetworks(function(n, m, s) { 
         console.log("hola", n, m, s);
         txt.append(n + " " + m + " " + s + "\n");
@@ -20,83 +21,124 @@ ui.addButton("Scan bluetooth", 10, 0).onClick(function() {
 });
 
 var btClient1;
-ui.addButton("Connect to bluetooth", 380, 0).onClick(function() {
-    //if you want to use the Bluetooth Address, use 
-    //network.bluetooth.connectSerial(macAddess, function(status) {});
+ui.addButton("Connect Hand", 290, 0).onClick(function() {
+
+    btClient1=network.bluetooth.connectSerial('98:D3:31:B2:DC:26', function(status) {
+        console.log("connected btClient1 " + status);
+
+        if (status) media.playSound("Hand_connected.wav");               //media.textToSpeech("Mano conectada"); 
+        //if (!status) media.playSound("Hand_disconnected.wav");           //media.textToSpeech("Mano desconectada"); 
+        
+    });
+    
+    /*
     btClient1 = network.bluetooth.connectSerial(function(status) {
         console.log("connected btClient1 " + status);
+        
+        if (status) media.playSound("Hand_connected.wav");               //media.textToSpeech("Mano conectada"); 
+        if (!status) media.playSound("Hand_disconnected.wav");           //media.textToSpeech("Mano desconectada"); 
     });
+    */
     
     btClient1.onNewData(function(data) {
         txt.text(data + "\n");
     });
 });
 
-ui.addButton("Disconnect", 680, 150).onClick(function() {
-    btClient1.disconnect();
-    btClient2.disconnect();
+ui.addButton("Disconnect", 10, 0).onClick(function() {
+    if(btClient1) btClient1.disconnect();
+    if(btClient2) btClient2.disconnect();
 });
 
 var input = ui.addInput("message", 10, 150, 400, 150);
 var send = ui.addButton("Send", 410, 150).onClick(function() {
     btClient1.send(input.getText() + "\n");
 });
-var send_OPEN = ui.addButton("OPEN", 10, 300).onClick(function() {
-    btClient1.send("ALL 50" + "\n");
-});
-var send_CLOSED = ui.addButton("CLOSED", 310, 300).onClick(function() {
-    btClient1.send("ALL -50" + "\n");
-});
-var send_thumbUp = ui.addButton("send_thumbUp", 610, 300).onClick(function() {
-    btClient1.send("ALL -50" + "\n" +  "E 140" + "\n");
-});
 
 
 var btClient2;
-ui.addButton("Connect to bluetooth", 380, 450).onClick(function() {
+ui.addButton("Connect FlexiGlove", 620, 0).onClick(function() {
     //if you want to use the Bluetooth Address, use 
     //network.bluetooth.connectSerial(macAddess, function(status) {});
+    
+    /*
     btClient2 = network.bluetooth.connectSerial(function(status) {
         console.log("connected btClient2 " + status);
+    });*/
+    
+    
+    btClient2=network.bluetooth.connectSerial('98:D3:31:30:1A:8C', function(status) {
+        console.log("connected btClient2 " + status);
+        if (status) media.playSound("Sensors_connected.wav");               //media.textToSpeech("Sensores conectados"); 
+        //if (!status) media.playSound("Sensors_disconnected.wav");           //media.textToSpeech("Sensores desconectados"); 
     });
+
     
     btClient2.onNewData(function(data) {
         
-        txt.text(data + "\n");
-        //console.log(data + "\n");
+        txt.text("Raw sensors: " + data + "\n");
         
        var tabla = data.split(" ");
-       
-       /*
-       for(i=1;i<=9;i=i*2){
-        servo_values[i] = parseInt(tabla[i]);
-       }
-       */
-       
+
         a=parseInt(tabla[1]);
         b=parseInt(tabla[3]);
         c=parseInt(tabla[5]);
         d=parseInt(tabla[7]);
         e=parseInt(tabla[9]);
         
+        
         var separator = ",";
         
-            a=convertSensor2servo(a,500,700,140,40);
-            b=convertSensor2servo(b,500,900,40,40);
-            c=convertSensor2servo(c,500,900,140,40);
-            d=convertSensor2servo(d,500,900,40,140);
-            e=convertSensor2servo(e,500,90,40,140);
+         //console.log( "Not map:" + a + separator + b + separator + c + separator + d + separator + e + "\t" );
         
-        console.log(a + separator + b + separator + c + separator + d + separator + e + "\n" );
-        btClient1.send(a + separator + b + separator + c + separator + d + separator + e + "\n" );
+        
+            a=map(a,500,900,160,0);
+            b=map(b,500,900,40,140);
+            c=map(c,500,900,140,20);
+            d=map(d,500,900,20,140);
+            e=map(e,500,900,40,180);
+       
+            a=a.toFixed(0);
+            b=b.toFixed(0);
+            c=c.toFixed(0);
+            d=d.toFixed(0);
+            e=e.toFixed(0);
+            
+            a_draw=map(a,140,40,40,140);
+            b_draw=b;
+            c_draw=map(c,140,40,40,140);
+            d_draw=d;
+            e_draw=e;            
+        
+        txt2.text("Actuators: " + a + separator + b + separator + c + separator + d + separator + e + "\n");
+        //console.log(a + separator + b + separator + c + separator + d + separator + e + "\n" );
+        if(btClient1) btClient1.send(a + separator + b + separator + c + separator + d + separator + e + "\n" );
         
         
         
     });
 });
 
+// *************************************************************************   buttons:
 
-ui.allowScroll(false);
+var send_OPEN = ui.addButton("OPEN", 10, 300).onClick(function() {
+    //btClient1.send("ALL 50" + "\n");
+    btClient1.send("140,40,140,40,40" + "\n");
+    
+});
+var send_CLOSED = ui.addButton("CLOSED", 310, 300).onClick(function() {
+    //btClient1.send("ALL -50" + "\n");
+    btClient1.send("40,140,40,140,140" + "\n");
+});
+var send_thumbUp = ui.addButton("send_thumbUp", 610, 300).onClick(function() {
+    btClient1.send("40,140,40,140,140" + "\n" +  "E 140" + "\n");
+});
+
+
+
+//*************************************************************************  Processing:
+
+ui.allowScroll(true);
 var processing = ui.addProcessing(10, 600, ui.screenWidth-20, 800, "P3D");
 var c; 
 
@@ -120,28 +162,29 @@ processing.draw(function(p) {
     count += 0.1;
     */
     
-    p.rect(0, p.height, 200, -a);
-    p.rect(220, p.height, 200, -b);
-    p.rect(440, p.height, 200, -c);
-    p.rect(660, p.height, 200, -d);
-    p.rect(880, p.height, 200, -e);
+    p.fill(2*a_draw,0,255-2*a_draw);
+    p.rect(0,   p.height, 200, -a_draw*6);
+    p.fill(2*b_draw,0,255-2*b_draw);
+    p.rect(220, p.height, 200, -b_draw*6);
+    p.fill(2*c_draw,0,255-2*c_draw);
+    p.rect(440, p.height, 200, -c_draw*6);
+    p.fill(2*d_draw,0,255-2*d_draw);
+    p.rect(660, p.height, 200, -d_draw*6);
+    p.fill(2*e_draw,0,255-2*e_draw);
+    p.rect(880, p.height, 200, -e_draw*6);
 });
 
 
-function map( x,  in_min,  in_max,  out_min,  out_max){
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+//**************************************************************************** Functions:
 
-
-function convertSensor2servo( sensor_val,  in_min,  in_max,  out_min,  out_max){
-		var x= sensor_val;
-//		int in_min = 450;
-//		int in_max = 900;
-//		int out_min = 10;
-//		int out_max = 170;
-		
-		var out = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-		if(out < out_min) return out_min; 
-		if(out > out_max) return out_max;
-		return out;
-	}
+ function  map( sensor_val,  in_min,  in_max,  out_min,  out_max){
+       // in_min start of range
+       // in_max end of range
+       // sensor_val the starting number between the range
+         var percentage = (sensor_val-in_min)/(in_max-in_min);
+         var out = (out_max-out_min)*percentage+out_min;
+       // out_min start of new range
+       // out_max end of new range
+       
+        return out;
+    }
