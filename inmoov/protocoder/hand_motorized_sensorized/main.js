2cc2efcb449ed24368f6d31c466569540601dd43
paclema <paclema@gmail.com>
2015-06-16 18:29:28 +0200

    Modularized functions and now arrays instead of variables for data of sensors and actuators

--- inmoov/protocoder/hand_motorized_sensorized/main.js
+++ inmoov/protocoder/hand_motorized_sensorized/main.js
@@ -1,6 +1,8 @@
 /* hand_controller
 
-Read from arduino by bluetooth diferent sensors and plot it on phone
+Read from arduino by bluetooth diferent sensors and plot it on phone. Then connect to another arduino by bluetooth to send data processend on protocoder app
+
+by Pablo Clemente (aka paclema)
 
     
 */
@@ -12,14 +14,21 @@ ui.screenMode("portrait");
 ui.screenMode("immersive");
 ui.allowScroll(true);
 
-var txt = ui.addText(10, 1400, ui.screenWidth, 50);
-var txt2 = ui.addText(10, 1450, ui.screenWidth, 50);
+// Main variables:
 
-//var servo_values;
-var a,b,c,d,e;
-var a_draw,b_draw,c_draw,d_draw,e_draw;
+var sensor_raw = [];
+var sensor_plot = [];
+var sensor_plot_draw = [];
 
-// *******************************************************************    Bluetooth conections:
+
+var sensor_raw_min = [ 500, 500, 500, 500, 500];
+var sensor_raw_max = [ 900, 900, 900, 900, 900];
+
+var actuator_min = [ 160, 40, 140, 20, 40];
+var actuator_max = [ 0, 140, 20, 140, 180];
+var actuator_dir = [ 0, 1, 0, 1, 1];
+
+//*****************************************************************************************   Bluetooth conections:
  
 
 //HAND:
@@ -67,7 +76,7 @@ ui.addCheckbox("Flexiglobe connected", 630, 70, 500, 100, false).onChange(functi
     
     if(!val){
         if(btClient2){
-            btClient1.disconnect();
+            btClient2.disconnect();
             media.playSound("Sensors_disconnected.wav"); 
         } 
     }
@@ -77,48 +86,14 @@ ui.addCheckbox("Flexiglobe connected", 630, 70, 500, 100, false).onChange(functi
         txt.text("Raw sensors: " + data + "\n");
         
         var tabla = data.split(" ");
-
-        a=parseInt(tabla[1]);
-        b=parseInt(tabla[3]);
-        c=parseInt(tabla[5]);
-        d=parseInt(tabla[7]);
-        e=parseInt(tabla[9]);
-        
-        
-        var separator = ",";
-        
-         //console.log( "Not map:" + a + separator + b + separator + c + separator + d + separator + e + "\t" );
-        
-        
-            a=map(a,500,900,160,0);
-            b=map(b,500,900,40,140);
-            c=map(c,500,900,140,20);
-            d=map(d,500,900,20,140);
-            e=map(e,500,900,40,180);
-       
-            a=a.toFixed(0);
-            b=b.toFixed(0);
-            c=c.toFixed(0);
-            d=d.toFixed(0);
-            e=e.toFixed(0);
-            
-            a_draw=map(a,140,40,40,140);
-            b_draw=b;
-            c_draw=map(c,140,40,40,140);
-            d_draw=d;
-            e_draw=e;            
-        
-        txt2.text("Actuators: " + a + separator + b + separator + c + separator + d + separator + e + "\n");
-        //console.log(a + separator + b + separator + c + separator + d + separator + e + "\n" );
-        if(btClient1) btClient1.send(a + separator + b + separator + c + separator + d + separator + e + "\n" );
-        
-        
+        for(var i=0;i<=4;i++) sensor_raw[i] = tabla[2*i+1];
+        Update_raw_sensors_data();
         
     });
 });
 
 
-// ********************************************************************   Buttons:
+//*****************************************************************************************   Buttons:
 
 var input = ui.addInput("command", 0, 10, 360, 150);
 var send = ui.addButton("Send", 370, 10).onClick(function() {
@@ -141,15 +116,16 @@ var send_thumbUp = ui.addButton("send_thumbUp", 610, 300).onClick(function() {
 
 
 
-//**********************************************************************   Plot Processing:
+//*****************************************************************************************   Plot Processing:
+var processing_heigth = 800;
+var processing = ui.addProcessing(10, 600, ui.screenWidth-20, processing_heigth, "P3D");
 
-var processing = ui.addProcessing(10, 600, ui.screenWidth-20, 800, "P3D");
-var c; 
+var plot_min = 0;
+var plot_max = processing_heigth-100;
 
 processing.setup(function(p) {
     p.background(1); 
     p.frameRate(25);
-    c = 100;
 });
 
 processing.draw(function(p) { 
@@ -158,37 +134,80 @@ processing.draw(function(p) {
     p.rect(0, 0, p.width, p.height);
     p.noStroke();
     p.fill(255);
-    /*
-    size = a / 500;
-    for(var i = 20; i < p.width - 20; i+=10 ) {
-        p.ellipse(i, p.height / 2 - 100+ b + x * p.sin(count + i), size, size);
-    }
-    count += 0.1;
-    */
+
     
-    p.fill(2*a_draw,0,255-2*a_draw);
-    p.rect(0,   p.height, 200, -a_draw*6);
-    p.fill(2*b_draw,0,255-2*b_draw);
-    p.rect(220, p.height, 200, -b_draw*6);
-    p.fill(2*c_draw,0,255-2*c_draw);
-    p.rect(440, p.height, 200, -c_draw*6);
-    p.fill(2*d_draw,0,255-2*d_draw);
-    p.rect(660, p.height, 200, -d_draw*6);
-    p.fill(2*e_draw,0,255-2*e_draw);
-    p.rect(880, p.height, 200, -e_draw*6);
+    for(var j=0;j<5;j++){
+        p.fill(2*sensor_plot_draw[j],0,255-2*sensor_plot_draw[j]);
+        p.rect(0+j*220,   p.height, 200, -sensor_plot_draw[j]);
+        
+    }
+        
 });
 
+//*****************************************************************************************   Texts:
 
-//***********************************************************************   Functions:
+var txt = ui.addText(10, 1400, ui.screenWidth, 50);
+var txt2 = ui.addText(10, 1450, ui.screenWidth, 50);
+var txt3 = ui.addText(10, 1500, ui.screenWidth, 50);
 
- function  map( sensor_val,  in_min,  in_max,  out_min,  out_max){
-       // in_min start of range
-       // in_max end of range
-       // sensor_val the starting number between the range
-         var percentage = (sensor_val-in_min)/(in_max-in_min);
-         var out = (out_max-out_min)*percentage+out_min;
-       // out_min start of new range
-       // out_max end of new range
+//*****************************************************************************************   Functions:
+
+function Update_raw_sensors_data(){
+    
+        // ParseInt raw data and copy to draw variables for processing plot:
+        for(var i=0; i<=4;i++){
+            sensor_raw[i] = parseInt(sensor_raw[i]);
+            sensor_plot[i] = sensor_raw[i];
+        } 
+        
+
+        // Map raw data to min/max actuator ends and fix to integer data:
+        for(var j=0; j<=4;j++){
+            if(actuator_dir[j])         sensor_raw[j] = map( sensor_raw[j], sensor_raw_min[j], sensor_raw_max[j], actuator_min[j], actuator_max[j]);      //Normal actuator direcction
+            else if(!actuator_dir[j])   sensor_raw[j] = map( sensor_raw[j], sensor_raw_max[j], sensor_raw_min[j], actuator_max[j], actuator_min[j]);      //Reversed actuator direction
+            
+            sensor_raw[j] = sensor_raw[j].toFixed(0);
+            
+        } 
+        
+
+       // Map plot data to min/max plot ends and fix to integer data:
+        for(var l=0;l<=4;l++){
+            sensor_plot[l] = map( sensor_plot[l], sensor_raw_min[l], sensor_raw_max[l], plot_min, plot_max);
+            sensor_plot[l] = sensor_plot[l].toFixed(0);
+            
+        }
+        
+        // Copy sensor_plot data to sensor_plot_draw data because of misbehaviour on processing plot:
+        for(var m=0; m<=4;m++)  sensor_plot_draw[m] = sensor_plot[m];
+
+        
+        // DEBUG:
+        var separator = ",";
+        txt2.text("Actuators: " + sensor_raw + "\n");
+        txt3.text("sensor_plot: " + sensor_plot + "\n");
+        
+        // Send processed data to actuators:
+        if(btClient1) btClient1.send(a + separator + b + separator + c + separator + d + separator + e + "\n" );
+        
+        
+}
+
+function update_sensors_ends(){
+    
+    
+    //  ******** ------------ TODO --------------------
+}
+
+function  map( sensor_val,  in_min,  in_max,  out_min,  out_max){
+    // in_min start of range
+    // in_max end of range
+    // sensor_val the starting number between the range
+    var percentage = (sensor_val-in_min)/(in_max-in_min);
+    var out = (out_max-out_min)*percentage+out_min;
+    // out_min start of new range
+    // out_max end of new range
        
-        return out;
-    }
+    return out;
+}
+
